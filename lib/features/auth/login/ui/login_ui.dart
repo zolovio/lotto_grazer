97510345002caea8cd340a54ab076f4c9d8915b1
Ahtsham0715 <ahtsham50743@gmail.com>
2022-11-04 16:44:15 +0500

    splash ui added

---
+++ lib/features/auth/login/ui/login_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginUi extends StatefulWidget {
+  const LoginUi({super.key});
+
+  @override
+  State<LoginUi> createState() => _LoginUiState();
+}
+
+class _LoginUiState extends State<LoginUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/features/auth/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lotto_grazer/features/auth/login/ui/login_ui.dart';
+
+class SplashUi extends StatefulWidget {
+  const SplashUi({super.key});
+
+  @override
+  State<SplashUi> createState() => _SplashUiState();
+}
+
+class _SplashUiState extends State<SplashUi> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        (() => Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const LoginUi(),
+            ))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Lotto Grazzer',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
