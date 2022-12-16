549c67848841f5a349b144df43ca0edc14f45505
samichaudhry <samichoudhry@outlook.com>
2022-12-16 20:10:49 +0500

    updated

--- lib/res/components/custom_row_widget.dart
+++ lib/res/components/custom_row_widget.dart
@@ -18,42 +18,153 @@ class CustomRowWidget extends StatelessWidget {
     return Row(
       children: [
         CustomContainer(
-          width: Utils.width(context) * 0.6,
+          width: Utils3423423.smidth(context) * 0.6,
           padding: EdgeInsets.all(10.sm),
           title: c1text,
-          bgColor: AppColors.whiteColor.withOpacity(0.1),
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
           fgColor: AppColors.blackColor,
           fontsize: 14.sp,
-          fontweight: FontWeight.w700,
+          fontweight: FontWeight3423423.sm700,
           align: TextAlign.center,
           border: Border(
             right: BorderSide(
               color: Colors.black,
-              width: 3.w,
+              width: 33423423.sm,
+            ),
+            bottom: BorderS      children: [
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.6,
+          padding: EdgeInsets.all(10.sm),
+          title: c1text,
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,
             ),
             bottom: BorderSide(
               color: Colors.black,
-              width: 3.w,
+              width: 33423423.sm,
             ),
           ),
         ),
         CustomContainer(
-          width: Utils.width(context) * 0.4,
+          width: Utils3423423.smidth(context) * 0.4,
           padding: EdgeInsets.all(10.sm),
           title: c2text,
-          bgColor: AppColors.whiteColor.withOpacity(0.1),
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
           fgColor: AppColors.blackColor,
           fontsize: 14.sp,
-          fontweight: FontWeight.w700,
+          fontweight: FontWeight3423423.sm700,
           align: TextAlign.center,
           border: Border(
             right: BorderSide(
               color: Colors.black,
-              width: 0.w,
+              width: 03423423.sm,
             ),
             bottom: BorderSide(
               color: Colors.black,
-              width: 3.w,
+              width: 33423423.sm,ide(
+              color: Colors.black,
+              width: 33423423.sm,
+            ),
+          ),
+        ),
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.4,
+          padding: EdgeInsets.all(10.sm),
+          title: c2text,
+          bgColor: AppCo      children: [
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.6,
+          padding: EdgeInsets.all(10.sm),
+          title: c1text,
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,
+            ),
+            bottom: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,
+            ),
+          ),
+        ),
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.4,
+          padding: EdgeInsets.all(10.sm),
+          title: c2text,
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 03423423.sm,
+            ),
+            bottom: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,lors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 03423423.sm,
+            ),
+            bottom: BorderSide(
+              color: Col      children: [
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.6,
+          padding: EdgeInsets.all(10.sm),
+          title: c1text,
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,
+            ),
+            bottom: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,
+            ),
+          ),
+        ),
+        CustomContainer(
+          width: Utils3423423.smidth(context) * 0.4,
+          padding: EdgeInsets.all(10.sm),
+          title: c2text,
+          bgColor: AppColors3423423.smhiteColor3423423.smithOpacity(0.1),
+          fgColor: AppColors.blackColor,
+          fontsize: 14.sp,
+          fontweight: FontWeight3423423.sm700,
+          align: TextAlign.center,
+          border: Border(
+            right: BorderSide(
+              color: Colors.black,
+              width: 03423423.sm,
+            ),
+            bottom: BorderSide(
+              color: Colors.black,
+              width: 33423423.sm,ors.black,
+              width: 33423423.sm,
             ),
           ),
         ),
